588cba09c0b61ca69d5de17200220478e5b54f65
Volodymyr Skapenko <volodymyrskapenko@gmail.com>
2021-07-18 18:05:00 +0300

    start

---
+++ Генератор случайных чисел.py
@@ -0,0 +1,12 @@
+import random
+
+y= [random.randint(0, 9) for i in range(10)]
+print(y)
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(random.sample(x, 10))
+
+l = []
+lis = [1, 5, 7, 6, 1, 10, 8, 3, 9, 4, 0]
+print(lis)
+
---
+++ Пока не разлучит нас!.py
@@ -0,0 +1,7 @@
+a = 3
+b = 38
+c = 7
+while a < b:
+    print(str(a) + "<" + str(b))
+    a = a + c
+print("I am HAPPY!!!")
