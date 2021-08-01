9b23627d07b6a6afa27993da0a2c15a65273beb2
VSkapenko <87611656+VSkapenko@users.noreply.github.com>
2021-08-01 23:10:23 +0300

    Add files via upload

---
+++ Welcome_Module.py
@@ -0,0 +1,6 @@
+def welcome():
+    name = input('Your name: ')
+    print('Welcome', name)
+
+if __name__ == '__main__':
+    print('My_module')
---
+++ exemple_module.py
@@ -0,0 +1,4 @@
+import Welcome_Module as wlc
+
+wlc.welcome()
+print('Hey, it working')
